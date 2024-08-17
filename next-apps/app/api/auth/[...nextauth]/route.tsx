e29e2ea1196ab1a0aa3a04c848c3c34ff662464f
abdullahalkawser <abdullahalkawser1@gmail.com>
2024-08-17 16:31:36 +0600

    Accessing Session on the Server

--- next-apps/app/api/auth/[...nextauth]/route.tsx
+++ next-apps/app/api/auth/[...nextauth]/route.tsx
@@ -3,14 +3,15 @@ import GoogleProvider from "next-auth/providers/google";
 
 console.log("GOOGLE_CLIENT_ID", process.env.GOOGLE_CLIENT_ID);
 console.log("GOOGLE_CLIENT_SECRET", process.env.GOOGLE_CLIENT_SECRET);
-
-const handelar = NextAuth({
+export const authOptions = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
   ],
-});
+}
+
+const handelar = NextAuth(authOptions);
 
 export { handelar as GET, handelar as POST };
--- next-apps/app/page.tsx
+++ next-apps/app/page.tsx
@@ -1,11 +1,15 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import Addtocard from './componant/Addtocard'
+import { getServerSession } from 'next-auth'
+import { authOptions } from './api/auth/[...nextauth]/route'
 
-export default function Home() {
+export default async function Home() {
+
+  const session = await getServerSession(authOptions)
   return (
 <div>
-  heloow next js
+  <h1>Hellow {session && <span>{session.user!.name}</span>}</h1>
   {/* <a href='/users'>aaaaaaaaaaa</a> */}
   <Link href='/users'>Users</Link>
   <Addtocard></Addtocard>
